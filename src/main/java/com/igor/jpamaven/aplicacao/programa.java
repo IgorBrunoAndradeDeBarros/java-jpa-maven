7cc016c755d68eb7d50771af5444f3cb92faff5a
igord <igordf2502@gmail.com>
2026-07-14 22:33:03 -0300

    criando persistencia de dados

--- src/main/java/com/igor/jpamaven/aplicacao/programa.java
+++ src/main/java/com/igor/jpamaven/aplicacao/programa.java
@@ -2,14 +2,26 @@ package com.igor.jpamaven.aplicacao;
 
 import com.igor.jpamaven.dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class programa {
 
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "carlitos", "carlo@gmail.com");
-        Pessoa p2 = new Pessoa(1, "carluro", "carluro@gmail.com");
-        Pessoa p3 = new Pessoa(1, "carbixo", "carbixo@gmail.com");
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+
+        Pessoa p1 = new Pessoa(null, "carlitos", "carlo@gmail.com");
+        Pessoa p2 = new Pessoa(null, "carluro", "carluro@gmail.com");
+        Pessoa p3 = new Pessoa(null, "carbixo", "carbixo@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+
+        System.out.println("Pronto");
     }
 }
