7ab086ea2f3cd7597c3f6b114199483f2638b0a8
igord <igordf2502@gmail.com>
2026-07-14 22:40:45 -0300

    refactor programaPersisteDados e adiciona programa de Busca de Dados

--- src/main/java/com/igor/jpamaven/aplicacao/programaBusca.java
+++ src/main/java/com/igor/jpamaven/aplicacao/programaBusca.java
@@ -6,22 +6,18 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class programa {
+public class programaBusca {
 
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(null, "carlitos", "carlo@gmail.com");
-        Pessoa p2 = new Pessoa(null, "carluro", "carluro@gmail.com");
-        Pessoa p3 = new Pessoa(null, "carbixo", "carbixo@gmail.com");
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
         EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+
+        Pessoa p = em.find(Pessoa.class, 1);
+        System.out.println(p);
 
         System.out.println("Pronto");
+        em.close();
+        emf.close();
     }
 }
---
+++ src/main/java/com/igor/jpamaven/aplicacao/programaPersisteDados.java
@@ -0,0 +1,29 @@
+package com.igor.jpamaven.aplicacao;
+
+import com.igor.jpamaven.dominio.Pessoa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class programaPersisteDados {
+
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa(null, "carlitos", "carlo@gmail.com");
+        Pessoa p2 = new Pessoa(null, "carluro", "carluro@gmail.com");
+        Pessoa p3 = new Pessoa(null, "carbixo", "carbixo@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+
+        System.out.println("Pronto");
+        em.close();
+        emf.close();
+    }
+}
