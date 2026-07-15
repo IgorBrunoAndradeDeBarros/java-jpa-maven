aaccee9b20710efe03452626c7e9442d113acf13
igord <igordf2502@gmail.com>
2026-07-14 22:24:45 -0300

    adiciona @Id na entidade Pessoa e configura Lombok

--- src/main/java/com/igor/jpamaven/dominio/Pessoa.java
+++ src/main/java/com/igor/jpamaven/dominio/Pessoa.java
@@ -1,52 +1,27 @@
 package com.igor.jpamaven.dominio;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pessoa implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
     private String email;
-
-    public Pessoa(){
-    }
-    public Pessoa(Integer id, String nome, String email){
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "Pessoa{" +
-                "nome='" + nome + '\'' +
-                ", id=" + id +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
